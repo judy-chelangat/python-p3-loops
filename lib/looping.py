945a97ed948652c633a91ab0fd0089761f3a1410
judy-chelangat <judysigilai75@gmail.com>
2023-08-16 14:06:17 +0300

    completed assignment

--- lib/looping.py
+++ lib/looping.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
+    i=10
+    while i >0 :
+        print(i)
+        i-=1
+        if i ==1:
+            print("Happy new year!")
     pass
 
 def square_integers(int_list):
-    # code goes here!
+    squared = [num*num for num in int_list]
+    return squared
     pass
 
 def fizzbuzz():
